10e2e043292eeeade74a25b6a6b7be4b1813fa7b
baher shehab <shehabmohamed2357@gmail.com>
2023-01-20 23:02:55 +0200

    added create post window

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ const observer = new IntersectionObserver((entry) => {
   const elem = entry[0]
   if (elem.isIntersecting) elem.target.classList.add("show")
 })
-
 const hiddenElem = document.querySelector(".hidden")
-observer.observe(hiddenElem)
+if(hiddenElem) observer.observe(hiddenElem)
+
+
+
+document.getElementById('create-post').addEventListener('click', () => {
+  const modal = document.querySelectorAll('.modal')
+  activeModal(modal)
+})
+document.getElementById('close-btn').addEventListener('click', () => {
+  const modal = document.querySelectorAll('.modal')
+  inactiveModel(modal)
+})
+
+function activeModal(modal) {
+  if(modal == null) return
+  modal.forEach(elem => elem.classList.add('active'))
+}
+function inactiveModel(modal) {
+  if(modal == null) return
+  modal.forEach(elem => elem.classList.remove('active'))
+}
