33cf38ba9aa5b0334462a3d4cca83592f6c1cbf7
baher shehab <shehabmohamed2357@gmail.com>
2023-01-23 20:18:59 +0200

    added signup/login pages

--- app.js
+++ app.js
@@ -9,29 +9,45 @@ if(hiddenElem) observer.observe(hiddenElem)
 let createPost_switcherFlag = true
 let editPost_flag = false // prevent createPost window activation in case editPost window is active
 
-document.getElementById('create-post').addEventListener('click', () => {
-  const modal = document.querySelectorAll('.create-modal')
-  if(!editPost_flag)
-  {
-    if(createPost_switcherFlag) activeModal(modal)
-    else inactiveModel(modal)
-  }
-})
-document.getElementById('edit-post').addEventListener('click', () => {
-  const modal = document.querySelectorAll('.edit-modal')
-  editPost_flag = true
-  activeModal(modal)
-})
+const createPost = document.getElementById('create-post')
+if(createPost)
+{
+  createPost.addEventListener('click', () => {
+    const modal = document.querySelectorAll('.create-modal')
+    if(!editPost_flag)
+    {
+      if(createPost_switcherFlag) activeModal(modal)
+      else inactiveModel(modal)
+    }
+  })
+}
+const editPost = document.getElementById('edit-post')
+if(editPost)
+{
+  editPost.addEventListener('click', () => {
+    const modal = document.querySelectorAll('.edit-modal')
+    editPost_flag = true
+    activeModal(modal)
+  })
+}
 
-document.getElementById('create-close-btn').addEventListener('click', () => {
-  const modal = document.querySelectorAll('.create-modal')
-  inactiveModel(modal)
-})
-document.getElementById('edit-close-btn').addEventListener('click', () => {
-  const modal = document.querySelectorAll('.edit-modal')
-  editPost_flag = false
-  inactiveModel(modal)
-})
+const createClose_btn = document.getElementById('create-close-btn')
+if(createClose_btn)
+{
+  createClose_btn.addEventListener('click', () => {
+    const modal = document.querySelectorAll('.create-modal')
+    inactiveModel(modal)
+  })
+}
+const editClose_btn = document.getElementById('edit-close-btn')
+if(editClose_btn)
+{
+  editClose_btn.addEventListener('click', () => {
+    const modal = document.querySelectorAll('.edit-modal')
+    editPost_flag = false
+    inactiveModel(modal)
+  })
+}
 
 function activeModal(modal) {
   if(modal == null) return
@@ -43,11 +59,3 @@ function inactiveModel(modal) {
   modal.forEach(elem => elem.classList.remove('active'))
   createPost_switcherFlag = true
 }
-
-
-
-
-
-
-
-
